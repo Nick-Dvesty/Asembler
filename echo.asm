e600eb538c698ed1c500a300cb58b6e3d84ad48e
Nick-Dvesty <106439014+Nick-Dvesty@users.noreply.github.com>
2024-03-12 13:42:16 +0700

    fix

--- echo.asm
+++ echo.asm
@@ -1,4 +1,4 @@
-.include "macro_syscall.asm"
+.include "mac_syscall.asm"
 
 .text
 main:
@@ -10,11 +10,11 @@ echo: #void echo()
 		li 	t0, 10
 		li 	t1, 127
 while_e:	readch
-		beq 	a0, t0, end_echo
+		beq 	a0, t0, end_e
 		bne	a0, t0, not_overflow
 		li 	a0, 0
 not_overflow:	printch a0
 		addi 	a0, a0, 1
 		printch a0
-		j 	while_echo
+		j 	while_e
 end_e:		ret
